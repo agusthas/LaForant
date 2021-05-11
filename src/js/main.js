96c5934afb5a138671bc6af53036ec112e9c5c19
agusthas <suzumantan@gmail.com>
2021-05-11 18:32:01 +0700

    lupa sampe mana

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 import '../scss/main.scss';
+import './jquery.min';
+
+// $(window).on('load', () => {
+//   setTimeout(() => {
+//     $('.loading').fadeOut();
+//   }, 1500);
+// });
+
+const openBtn = $('#js-hamburger-open');
+const closeBtn = $('#js-hamburger-close');
+const mobileMenu = $('.mobile-menu');
+
+openBtn.on('click', () => {
+  $('body').css('overflow', 'hidden');
+  mobileMenu.removeClass('mobile-menu--hidden');
+  mobileMenu.addClass('mobile-menu--show');
+});
+
+closeBtn.on('click', () => {
+  $('body').css('overflow', 'auto');
+  mobileMenu.removeClass('mobile-menu--show');
+  mobileMenu.addClass('mobile-menu--hidden');
+});
