ae84874ac90585283769871947b6c8019a208ea9
agusthas <suzumantan@gmail.com>
2021-05-14 21:21:42 +0700

    refactor: update url to avoid conflict in Vite

--- src/js/content/Updates.js
+++ src/js/content/Updates.js
@@ -1,25 +1,25 @@
 const Updates = [
   {
     title: 'NEW AGENT: ASTRA',
-    img: '/public/assets/images/new-agent-astra-reveal.jpg',
+    img: '/assets/images/new-agent-astra-reveal.jpg',
     date: '05/14/2021',
     link: 'https://www.youtube.com/watch?v=-ylVnuPWlJM',
   },
   {
     title: 'MAP REVEAL',
-    img: '/public/assets/images/new-map-reveal.jpg',
+    img: '/assets/images/new-map-reveal.jpg',
     date: '05/05/2021',
     link: 'https://www.youtube.com/watch?v=HuI3qdaL-jI',
   },
   {
     title: 'RANKED GAMEPLAY',
-    img: '/public/assets/images/ranked-gameplay-reveal.jpg',
+    img: '/assets/images/ranked-gameplay-reveal.jpg',
     date: '05/01/2021',
     link: 'https://www.youtube.com/watch?v=WV1LCOHoDwA',
   },
   {
     title: 'COMMUNITY SPOTLIGHT #6',
-    img: '/public/assets/images/community-spotlight.jpg',
+    img: '/assets/images/community-spotlight.jpg',
     date: '04/04/2021',
     link: 'https://playvalorant.com/en-us/news/announcements/community-spotlight-6/',
   },
