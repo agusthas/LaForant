3c1518024f11c5ec5688f837187ec63ecf21979a
agusthas <suzumantan@gmail.com>
2021-05-15 21:45:31 +0700

    Navbar: dropdown mobile Fixes #4

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -1,19 +1,35 @@
 const Navbar = () => {
-  const openBtn = $('#js-hamburger-open');
-  const closeBtn = $('#js-hamburger-close');
-  const mobileMenu = $('.mobile-menu');
+  const dropdownToggler = document.querySelectorAll(
+    '.navbar__link-text[data-dd-toggler]',
+  );
 
-  openBtn.on('click', () => {
-    $('body').css('overflow', 'hidden');
-    mobileMenu.removeClass('mobile-menu--hidden');
-    mobileMenu.addClass('mobile-menu--show');
+  const sidebarOpen = document.querySelector('#js-hamburger--open');
+  const sidebarClose = document.querySelector('#js-hamburger--close');
+  const sidebar = document.querySelector('#js-sidebar');
+
+  dropdownToggler.forEach((t) => {
+    t.addEventListener('click', (e) => {
+      const curr = e.target;
+
+      curr.classList.toggle('navbar__link-text--active');
+      $(curr).siblings('.dropdown').toggleClass('dropdown--show');
+    });
   });
 
-  closeBtn.on('click', () => {
-    $('body').css('overflow', 'auto');
-    mobileMenu.removeClass('mobile-menu--show');
-    mobileMenu.addClass('mobile-menu--hidden');
-  });
+  try {
+    sidebarOpen.addEventListener('click', (e) => {
+      e.stopPropagation();
+      sidebar.classList.toggle('show');
+      document.body.style.overflow = 'hidden';
+    });
+    sidebarClose.addEventListener('click', (e) => {
+      e.stopPropagation();
+      sidebar.classList.toggle('show');
+      document.body.style.overflow = 'auto';
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default Navbar;
