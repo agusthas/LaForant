07ceb0d6f0ec55fa05b5bd91ca627376792181da
agusthas <suzumantan@gmail.com>
2021-05-21 21:25:02 +0700

    feat: add new agents

--- src/js/contents/AgentsContent.js
+++ src/js/contents/AgentsContent.js
@@ -5,6 +5,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=viper',
     image: '/assets/images/viper/viper.png',
     quote: 'The American chemist',
+    role: 'CONTROLLER',
+    role_symbol: '/assets/images/class/controller-class-symbol.png',
     bio: "Viper deploys an array of poisonous chemical devices to control the battlefield and cripple the enemy's vision. If the toxins don't kill her prey, her mind games surely will",
     ability: [
       {
@@ -43,6 +45,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=sage',
     image: '/assets/images/sage/sage.png',
     quote: 'The stronghold of china',
+    role: 'SENTINEL',
+    role_symbol: '/assets/images/class/sentinel-class-symbol.png',
     bio: 'Sage creates safety for herself and her team wherever she goes. Able to revive fallen friends and stave off aggressive pushes, she provides a calm center to a hellish fight',
     ability: [
       {
@@ -81,6 +85,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=jett',
     image: '/assets/images/jett/jett.png',
     quote: 'The Korean Tornado',
+    role: 'DUELIST',
+    role_symbol: '/assets/images/class/duelist-class-symbol.png',
     bio: "Jett's agile and evasive fighting style lets her take risks no one else can. She runs circles around every skirmish, cutting enemies before they even know what hit them.",
     ability: [
       {
@@ -118,6 +124,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=skye',
     image: '/assets/images/skye/skye.png',
     quote: 'The Guardian Of Australia',
+    role: 'INITIATOR',
+    role_symbol: '/assets/images/class/initiator-class-symbol.png',
     bio: "Skye and her band of beasts trail-blaze the way through hostile territory. With her creations hampering the enemy, and her power to heal others, the team is strongest and safest by Skye's side.",
     ability: [
       {
@@ -155,6 +163,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=astra',
     image: '/assets/images/astra/astra.png',
     quote: 'Ghanaian Predicter',
+    role: 'CONTROLLER',
+    role_symbol: '/assets/images/class/controller-class-symbol.png',
     bio: 'Ghanaian Agent Astra harnesses the energies of the cosmos to reshape battlefields to her whim. With full command of her astral form and a talent for deep strategic foresight, she’s always eons ahead of her enemy’s next move.',
     ability: [
       {
@@ -193,6 +203,8 @@ const AgentsContent = [
     link: '/pages/agents.html?agent=reyna',
     image: '/assets/images/reyna/reyna.png',
     quote: 'The Mexican Vampire',
+    role: 'DUELIST',
+    role_symbol: '/assets/images/class/duelist-class-symbol.png',
     bio: 'Forged in the heart of Mexico, Reyna dominates single combat, popping off with each kill she scores. Her capability is only limited by her raw skill, making her highly dependent on performance.',
     ability: [
       {
@@ -222,6 +234,86 @@ const AgentsContent = [
         ability_logo: '/assets/images/reyna/reyna-empress.png',
         ability_description:
           'Enter a frenzy for 30 seconds, increasing firing, equip and reload speeds by 15%. Grants infinite charges of Soul Harvest abilities. Scoring a kill fully refreshes the duration.',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'KILLJOY',
+    link: '/pages/agents.html?agent=killjoy',
+    image: '/assets/images/killjoy/killjoy.png',
+    quote: 'The genius of Germany',
+    role: 'SENTINEL',
+    role_symbol: '/assets/images/class/sentinel-class-symbol.png',
+    bio: "Killjoy secures the battlefield with ease using her arsenal of inventions. If the damage from her gear doesn't stop her enemies, her robots' debuff will help make short work of them.",
+    ability: [
+      {
+        id: 0,
+        ability_name: 'ALARMBOT',
+        ability_logo: '/assets/images/killjoy/killjoy-alarmbot.png',
+        ability_description:
+          'EQUIP a covert Alarmbot. FIRE to deploy a bot that hunts down enemies that get in range. After reaching its target, the bot explodes, applying Vulernable. HOLD EQUIP to recall a deployed bot.',
+      },
+      {
+        id: 1,
+        ability_name: 'TURRET',
+        ability_logo: '/assets/images/killjoy/killjoy-turret.png',
+        ability_description:
+          'EQUIP a Turret. FIRE to deploy a turret that fires at enemies in a 180 degree cone. HOLD EQUIP to recall the deployed turret',
+      },
+      {
+        id: 2,
+        ability_name: 'NANOSWARM',
+        ability_logo: '/assets/images/killjoy/killjoy-nanoswarm.png',
+        ability_description:
+          'EQUIP a Nanoswarm grenade. FIRE to throw the grenade. Upon landing, the Nanoswarm goes covert. ACTIVATE the Nanoswarm to deploy a damaging swarm of nanobots',
+      },
+      {
+        id: 3,
+        ability_name: 'LOCKDOWN',
+        ability_logo: '/assets/images/killjoy/killjoy-lockdown.png',
+        ability_description:
+          'EQUIP the Lockdown device. FIRE to deploy the device. After a long windup, the device Detains all enemies caught in the radius. The device can be destroyed by enemies.',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'RAZE',
+    link: '/pages/agents.html?agent=raze',
+    image: '/assets/images/raze/raze.png',
+    quote: 'Explode Queen of Brazil',
+    role: 'DUELIST',
+    role_symbol: '/assets/images/class/duelist-class-symbol.png',
+    bio: 'Raze explodes out of Brazil with her big personality and big guns. With her blunt-force-trauma playstyle, she excels at flushing entrenched enemies and clearing tight spaces with a generous dose of “boom.”',
+    ability: [
+      {
+        id: 0,
+        ability_name: 'BOOM BOT',
+        ability_logo: '/assets/images/raze/raze-boom-bot.png',
+        ability_description:
+          'EQUIP a Boom Bot. FIRE will deploy the bot, causing it to travel in a straight line on the ground, bouncing off walls. The Boom Bot will lock on to any enemies in its frontal cone and chase them, exploding for heavy damage if it reaches them.',
+      },
+      {
+        id: 1,
+        ability_name: 'BLAST PACK',
+        ability_logo: '/assets/images/raze/raze-blast-pack.png',
+        ability_description:
+          "INSTANTLY throw a Blast Pack that will stick to surfaces. RE-USE the ability after deployment to detonate, damaging and moving anything hit. Raze isn't damaged by this ability, but will still take fall damage if launched up far enough.",
+      },
+      {
+        id: 2,
+        ability_name: 'PAINT SHELLS',
+        ability_logo: '/assets/images/raze/raze-paint-shells.png',
+        ability_description:
+          'EQUIP a cluster grenade. FIRE to throw the grenade, which does damage and creates sub-munitions, each doing damage to anyone in their range.',
+      },
+      {
+        id: 3,
+        ability_name: 'SHOWSTOPPER',
+        ability_logo: '/assets/images/raze/raze-showstopper.png',
+        ability_description:
+          'EQUIP a rocket launcher. FIRE shoots a rocket that does massive area damage on contact with anything.',
       },
     ],
   },
