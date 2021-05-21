fb4dd77fcf11c11d658ff7bbe54c51a95caad617
agusthas <suzumantan@gmail.com>
2021-05-21 19:08:49 +0700

    refactor: organize public folder

--- src/js/contents/MapsContent.js
+++ src/js/contents/MapsContent.js
@@ -2,31 +2,31 @@ const MapsContent = [
   {
     name: 'breeze',
     link: '/pages/maps.html?map=breeze',
-    image: '/assets/images/breeze.png',
+    image: '/assets/images/maps/breeze.png',
     desc: 'Our latest new map. Featuring historic ruins and seaside caves on a tropical islands. Watch your flanks and beware of this map open spaces and long range engangements. As long as you got it covered, it will be a Breeze.',
   },
   {
     name: 'icebox',
     link: '/pages/maps.html?map=icebox',
-    image: '/assets/images/icebox.png',
+    image: '/assets/images/maps/icebox.png',
     desc: 'Chill Chill Chill! Your next map is covered in snow (be sure to bring jacket!). Traversing this map requires your ability to make use of those ziplines. Use the ziplines and they will never see you coming!',
   },
   {
     name: 'bind',
     link: '/pages/maps.html?map=bind',
-    image: '/assets/images/bind.png',
+    image: '/assets/images/maps/bind.png',
     desc: "As the name suggest, two sites binded with each other. There's no middleground here. Choose your side wisely and plan your attack swiftly. Remember... Always watch your flanks.",
   },
   {
     name: 'haven',
     link: '/pages/maps.html?map=haven',
-    image: '/assets/images/haven.png',
+    image: '/assets/images/maps/haven.png',
     desc: 'Haven.',
   },
   {
     name: 'ascent',
     link: '/pages/maps.html?map=ascent',
-    image: '/assets/images/ascent.png',
+    image: '/assets/images/maps/ascent.png',
     desc: 'Ascent.',
   },
   {
--- src/js/contents/UpdatesContent.js
+++ src/js/contents/UpdatesContent.js
@@ -1,25 +1,25 @@
 const UpdatesContent = [
   {
     title: 'NEW AGENT: ASTRA',
-    img: '/assets/images/new-agent-astra-reveal.jpg',
+    img: '/assets/images/updates/new-agent-astra-reveal.jpg',
     date: '05/14/2021',
     link: 'https://www.youtube.com/watch?v=-ylVnuPWlJM',
   },
   {
     title: 'MAP REVEAL',
-    img: '/assets/images/new-map-reveal.jpg',
+    img: '/assets/images/updates/new-map-reveal.jpg',
     date: '05/05/2021',
     link: 'https://www.youtube.com/watch?v=HuI3qdaL-jI',
   },
   {
     title: 'RANKED GAMEPLAY',
-    img: '/assets/images/ranked-gameplay-reveal.jpg',
+    img: '/assets/images/updates/ranked-gameplay-reveal.jpg',
     date: '05/01/2021',
     link: 'https://www.youtube.com/watch?v=WV1LCOHoDwA',
   },
   {
     title: 'COMMUNITY SPOTLIGHT #6',
-    img: '/assets/images/community-spotlight.jpg',
+    img: '/assets/images/updates/community-spotlight.jpg',
     date: '04/04/2021',
     link: 'https://playvalorant.com/en-us/news/announcements/community-spotlight-6/',
   },
